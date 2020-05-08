a1df07b62bfafeb76bfcd17f930fbc998b7c96c3
= <nidhalbacc@gmail.com>
2020-05-08 23:12:24 +0200

    fixed py versions

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = []
 
 setup_requirements = ['pytest-runner', ]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = ['pytest>=3.6', ]
 
 setup(
     author="Nidhal Baccouri",
@@ -25,8 +25,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
@@ -48,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nidhaloff/gpx_converter',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
