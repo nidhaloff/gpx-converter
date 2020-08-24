73d4ce4494635a7c650039d33731587f98c0ff98
= <nidhalbacc@gmail.com>
2020-08-24 19:56:54 +0200

    updated docs

--- gpx_converter/__init__.py
+++ gpx_converter/__init__.py
@@ -2,6 +2,6 @@ from .base import Converter
 
 __author__ = """Nidhal Baccouri"""
 __email__ = 'nidhalbacc@gmail.com'
-__version__ = '1.6.8'
+__version__ = '1.6.9'
 
 __all__ = [Converter]
--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nidhaloff/gpx_converter',
-    version='1.6.8',
+    version='1.6.9',
     zip_safe=False,
 )
