10ec060c9c435ff5052de1f77135b9325b25b936
nidhal baccouri <nidhalbacc@gmail.com>
2020-12-27 13:56:49 +0100

    refactored and added test files

--- examples/csv_to_gpx.py
+++ examples/csv_to_gpx.py
@@ -2,7 +2,7 @@ from gpx_converter import Converter
 
 
 # convert csv to gpx
-Converter(input_file='test_data/test1.csv').csv_to_gpx(lats_colname='latitudes',
-                                                       longs_colname='longitudes',
-                                                       output_file='test_data/converted.gpx')
+Converter(input_file='test_data/test_file.csv').csv_to_gpx(lats_colname='latitude',
+                                                            longs_colname='longitude',
+                                                            output_file='test_data/converted.gpx')
 
--- examples/gpx_to_csv.py
+++ examples/gpx_to_csv.py
@@ -1,4 +1,4 @@
 from gpx_converter import Converter
 
 # convert gpx to a csv file
-Converter(input_file='test_data/test1.gpx').gpx_to_csv(output_file='test_data/converted_to_csv.csv')
+Converter(input_file='test_data/test_file.gpx').gpx_to_csv(output_file='test_data/test_file.csv')
