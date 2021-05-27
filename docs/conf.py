4b22f9825588ce42aa62248cbcbc20fa01a5d786
nidhal baccouri <nidhalbacc@gmail.com>
2021-05-27 11:02:52 +0200

    added sphnx copy btn

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ html_logo = "../assets/icon.png"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_copybutton']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_copybutton', 'sphinx.ext.autosectionlabel']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
